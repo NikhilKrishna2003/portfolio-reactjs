f3600f66115bc8bcdd01aa871ffe07e25f4d8127
B NIKHIL KRISHNA <162432391+NikhilKrishna2003@users.noreply.github.com>
2025-01-04 13:08:29 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
-import AboutSection from "./components/aboutSection";
+import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
